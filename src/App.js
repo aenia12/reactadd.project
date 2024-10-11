075c0feab2e3dbf564c5e7790cb1d29f1b5cec94
aenia12 <aenia12@gmail.com>
2024-10-11 17:50:30 -0400

    Worked on rest of files and functionality.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <strong>Dictionary</strong>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project is coded by{" "}
+            <a
+              href="https://www.shecodes.io/graduates/77665-aenia-amin"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Aenia Amin
+            </a>{" "}
+            and is hosted on{" "}
+            <a
+              href="https://github.com/aenia12/reactadd.project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub{" "}
+            </a>
+            and{" "}
+            <a
+              href="https://fanciful-brigadeiros-5ccd44.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify.
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
