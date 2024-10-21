b51ab596838f75f4f97e33ad22dbafad8008bb56
aenia12 <aenia12@gmail.com>
2024-10-21 16:18:57 -0400

    Worked on Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,10 +9,12 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <div className="definition">{definition.definition}</div>
-
-            <div className="example">{definition.example}</div>
-            <Synonyms synonyms={definition.synonyms} />
+            <p>
+              <div className="definition">{definition.definition}</div>
+              <br />
+              <div className="example">{definition.example}</div>
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
           </div>
         );
       })}
